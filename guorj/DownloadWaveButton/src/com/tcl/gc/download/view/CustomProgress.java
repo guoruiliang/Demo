bcc92ca9254a71e552fbdddf54879da62976e487
guoruiliang <guoruiliang@qq.com>
2015-07-15 01:15:08 +0800

    fix textsize bug

--- guorj/DownloadWaveButton/src/com/tcl/gc/download/view/CustomProgress.java
+++ guorj/DownloadWaveButton/src/com/tcl/gc/download/view/CustomProgress.java
@@ -5,6 +5,7 @@ import com.tcl.gc.download.R;
 import android.content.Context;
 import android.content.res.TypedArray;
 import android.util.AttributeSet;
+import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.widget.ImageView;
@@ -52,7 +53,7 @@ public class CustomProgress extends RelativeLayout {
 		// 属性的名称是styleable中的名称+“_”+属性名称
 		TypedArray array = context.obtainStyledAttributes(attrs, R.styleable.CustomProgressBar);
 		textColor = array.getColor(R.styleable.CustomProgressBar_android_textColor, 0XFF000000); // 默认黑色
-		textSize = array.getDimension(R.styleable.CustomProgressBar_android_textSize,  DensityUtil.dip2px(context, 14));//文字大小，默认14dp
+		textSize = array.getDimension(R.styleable.CustomProgressBar_android_textSize,  14);//文字大小，默认14dp
 		imageWidth = array.getDimension(R.styleable.CustomProgressBar_image_width, defautlSize);
 		imageHeight = array.getDimension(R.styleable.CustomProgressBar_image_height, defautlSize);
 		array.recycle(); // 一定要调用，否则这次的设定会对下次的使用造成影响
@@ -63,8 +64,7 @@ public class CustomProgress extends RelativeLayout {
 		imageParams.addRule(RelativeLayout.CENTER_HORIZONTAL);
 		imageParams.addRule(RelativeLayout.ALIGN_PARENT_TOP);
 		mImageView.setLayoutParams(imageParams);
-		
-		mTextView.setTextSize(textSize);
+		mTextView.setTextSize(DensityUtil.px2dip(context, textSize));
 		mTextView.setTextColor(textColor);
 		
 		mDownloadWaveView.setLayoutParams(imageParams);
